1a440f7a1e2780e477f12e01e30cca2bbd18588e
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2022-11-28 21:48:43 +0200

    initialize df function contains main features

--- data/collecting_data.py
+++ data/collecting_data.py
@@ -5,3 +5,34 @@ import pandas as pd
 import re
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def initialize_df():
+    '''Create the necessary dataframes for extracting features
+    from the files.
+    '''
+    global features
+
+    features = {
+        'name': None,
+        'model': None,
+        'price': None,
+        'color': None,
+        'fuel_type': None,
+        'origin_car': None,
+        'car_license': None,
+        'lime_type': None,
+        'glass': None,
+        'motor_power': None,
+        'car_speedometer': None,
+        'passengers': None,
+        'payment_method': None,
+        'displayed': None,
+        'ex_owners': None,
+        'additional_info': None,
+        'insurance_third': None,
+        'supplementary_body': None,
+        'total': None,
+        'ads_status': None,
+        'ads_start_data': None,
+        'ads_end_data': None,
+        }
